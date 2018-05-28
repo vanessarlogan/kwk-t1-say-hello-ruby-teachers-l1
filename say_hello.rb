088313949779f79f03770efb77193c5dbdd6561e
vanessarlogan Wentzloff <vanessarlogan@gmail.com>
2018-05-28 14:28:15 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
 # Build your say_hello method here
 def say_hello(name)
   puts "Hello #{name}!"
-  if name = ""
-    name = "Ruby Programmer"
+  if name == ""
+    name == "Ruby Programmer"
 end
 
-say_hello("Vanessa")
+say_hello("")
